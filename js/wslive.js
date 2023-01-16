8fba4f1a6177bd1f117d6b99994ceb43557aac6c
diyanqi <diyanqi07@gmail.com>
2023-01-16 16:39:51 +0800

    ws test

--- js/wslive.js
+++ js/wslive.js
@@ -5,15 +5,18 @@ var ws = new WebSocket("ws://api.oiso.cf:2096/ws/live");
 ws.onmessage = function (event) {
     // 每 1 秒更新一次
     // {"spider_status":spider_status,"chat_msg":chat_msg,"song_msg":song_msg,"online_num":online_num,"online_list":online_list}
-    var data = JSON.parse(event.data);
-    // 更新在线人数
-    console.log(data.online_num);
-    // 更新在线列表
-    console.log(data.online_list);
-    // 更新聊天室
-    console.log(data.chat_msg);
-    // 更新歌曲列表
-    console.log(data.song_msg);
-    // 更新爬虫状态
-    console.log(data.spider_status);
+    // var data = JSON.parse(event.data);
+    // // 更新在线人数
+    // console.log(data.online_num);
+    // // 更新在线列表
+    // console.log(data.online_list);
+    // // 更新聊天室
+    // console.log(data.chat_msg);
+    // // 更新歌曲列表
+    // console.log(data.song_msg);
+    // // 更新爬虫状态
+    // console.log(data.spider_status);
+}
+ws.onopen = function (event) {
+    console.log("连接成功");
 }
