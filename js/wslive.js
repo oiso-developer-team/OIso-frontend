23955b27f3dab81cb680a24e118ef4fc75ba5451
diyanqi <diyanqi07@gmail.com>
2023-01-16 19:55:13 +0800

    test

--- js/wslive.js
+++ js/wslive.js
@@ -1,6 +1,8 @@
 // 主页的聊天室什么的 使用 socket.io 进行更新
 
 var socket = io.connect('https://api.oiso.cf:2096/');
+// 要带上cookie
+socket.request.headers.cookie = document.cookie;
 socket.on('connect', function() {
     console.log('Connected!');
 });
