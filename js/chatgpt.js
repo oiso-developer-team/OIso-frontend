be6c9c30cad5a302e7cb341fb7bdaaf6ddc87eef
diyanqi <diyanqi07@gmail.com>
2023-02-19 12:21:48 +0800

    chatgpt4

--- js/chatgpt.js
+++ js/chatgpt.js
@@ -7,6 +7,28 @@ fetch(window['api'] + "/profile", {
 }).then(function (data) {
     if (data != `False`) {
         window['profile'] = JSON.parse(data);
+        var startmsg = document.getElementById("startmsg");
+        // 早上、中午、下午、晚上？
+        var now = new Date();
+        var hour = now.getHours();
+        if (hour < 6) {
+            startmsg.innerHTML = "早上好，";
+        } else if (hour < 9) {
+            startmsg.innerHTML = "早上好，";
+        } else if (hour < 12) {
+            startmsg.innerHTML = "上午好，";
+        } else if (hour < 14) {
+            startmsg.innerHTML = "中午好，";
+        } else if (hour < 17) {
+            startmsg.innerHTML = "下午好，";
+        } else if (hour < 19) {
+            startmsg.innerHTML = "傍晚好，";
+        } else if (hour < 22) {
+            startmsg.innerHTML = "晚上好，";
+        } else {
+            startmsg.innerHTML = "夜里好，";
+        }
+        startmsg.innerHTML += window['profile']['name'] + "，有什么问题尽管问我！";
         namespace = '/chatgpt';
         var socket = io.connect(window['api'] + namespace, {
             transportOptions: {
